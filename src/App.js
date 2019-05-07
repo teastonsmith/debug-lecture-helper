3e59c0c6c0ce9dad47ed2c4cb1783c2ae1aa6c5d
teastonsmith2 <t.easton.smith2@gmail.com>
2019-05-07 10:37:15 -0600

    second commit

--- src/App.js
+++ src/App.js
@@ -2,28 +2,27 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      timer: 0
-    }
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			timer: 0,
+		};
+	}
 
-  componenetDidMount() {
-    console.log('componentDidMount method invoked...')
-    const incrementTimer = () => this.setState({ timer: this.state.timer + 1 })
-    setInterval(incrementTimer, 1000)
-  }
+	componentDidMount() {
+		console.log('componentDidMount method invoked...');
+		const incrementTimer = () => this.setState({ timer: this.state.timer + 1 });
+		setInterval(incrementTimer, 1000);
+	}
 
-
-  render() {
-    return (
-      <div className="App">
-        <p>How many seconds have you been on my app?</p>
-        <p>{this.state.timer} seconds</p>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className='App'>
+				<p>How many seconds have you been on my app?</p>
+				<p>{this.state.timer} seconds</p>
+			</div>
+		);
+	}
 }
 
 export default App;
